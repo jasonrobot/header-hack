ba612bd8fbe113482ab58fdda4f33ad61fd1bf5b
Jason Howell <jasonrobot64@gmail.com>
2018-08-14 23:33:03 -0700

    Cleaned up code, updated readme.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # header-hack
+
 A bad hack of a plugin that allows you to set custom request headers with selenium. Like, a really bad hack.
+
+The primary usecase of this is for selenium tests in developments
+
+## Usage
+
+1. Load the extension in the browser.
+2. Visit http://name.value.example.com
+   1. replace "name" and "value" with the name and value of the header you want added
+3. That's it! That header will be added to every request that the browser makes.
+
+
--- background.js
+++ background.js
@@ -1,20 +1,5 @@
 var extraHeaders = {};
 
-//listen for the message to add a header to modify
-chrome.runtime.onMessageExternal.addListener(
-  function(message, sender, sendResponse) {
-    console.log('Got a message: ' + message);
-    alert('something')
-  }
-);
-
-chrome.runtime.onMessage.addListener(
-  function(message, sender, sendResponse) {
-    console.log('Got a message: ' + message);
-    alert('something')
-  }
-);
-
 chrome.webRequest.onBeforeSendHeaders.addListener(
   function(data) {
     console.log(extraHeaders.length);
@@ -22,7 +7,7 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
       console.log(entry + ": " + extraHeaders[entry]);
       data.requestHeaders.push({name: entry, value: extraHeaders[entry]});
     }
-    console.log('modified!!!');
+    console.log('headers were modified');
     return {requestHeaders: data.requestHeaders};
   },
   {urls: ['<all_urls>']},
@@ -35,18 +20,10 @@ chrome.webRequest.onBeforeRequest.addListener(
     var regex = /http:\/\/([\w\-]*)\.([\w\-]*)\.example\.com/;
     var result = data.url.match(regex);
     if (result) {
-        // extraHeaders.push({name: result[1], value: result[2]});
         extraHeaders[result[1]] = result[2];
-        console.log('added a header')
+        console.log('added a header');
     }
   },
   {urls: ['http://*.example.com/*']},
   ['blocking']
 );
-
-// chrome.webRequest.onCompleted.addListener(
-//   function(){
-//     console.log('Request completed!')
-//   },
-//   {urls: []}
-// )
